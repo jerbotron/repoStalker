8952f4eadafcb6bc3e03cf7b2a5bde61f3ec5f8d
Jeremy <jer.wang25@gmail.com>
2016-11-20 19:44:05 -0600

    first commit

---
+++ src/main/java/com/bypassmobile/octo/adapters/UserCardAdapter.java
@@ -0,0 +1,8 @@
+package com.bypassmobile.octo.adapters;
+
+/**
+ * Created by jeremy on 20/11/16.
+ */
+
+public class UserCardAdapter {
+}
---
+++ src/main/java/com/bypassmobile/octo/image/CircleImageTransformation.java
@@ -0,0 +1,8 @@
+package com.bypassmobile.octo.image;
+
+/**
+ * Created by jeremy on 20/11/16.
+ */
+
+public class CircleImageTransformation {
+}
